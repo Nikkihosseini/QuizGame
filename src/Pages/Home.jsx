be08fd8e89895e3c9c3fa1dbc9b6f13c43579a32
Nikkihosseini <niekal00@gmail.com>
2025-09-11 16:56:44 +0330

    feat: add initial Home component structure

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -8,13 +8,47 @@ export default function Home(){
                     <h1>fun quiz game</h1>
                 </div>
                 <div>
-                    <div></div>
-                    <div></div>
-                    <div></div>
-                    <div></div>
+                    <div>
+                        <span className="material-symbols-outlined">cruelty_free</span>
+                        <span>Animals</span>
+                    </div>
+                    <div>
+                        <span className="material-symbols-outlined">sports_soccer</span>
+                        <span>Sport</span>
+                    </div>
+                    <div>
+                        <span className="material-symbols-outlined">science</span>
+                        <span>Science</span>
+                    </div>
+                    <div>
+                        <span className="material-symbols-outlined">manufacturing</span>
+                        <span>History</span>
+                    </div>
                 </div>
-                <div></div>
-                <div></div>
+                <div>
+                    <div>
+                        <span>Difficulty:</span>
+                        <select name="Difficulty" id="Difficulty">
+                            <option value="easy">Easy</option>
+                            <option value="medium">Medium</option>
+                            <option value="hard">Hard</option>
+                        </select>
+                    </div>
+                    <div>
+                        <span>Question:</span>
+                        <select name="Question" id="Question">
+                            <option value="10">10</option>
+                            <option value="15">15</option>
+                            <option value="20">20</option>
+                            <option value="25">25</option>
+                            <option value="30">30</option>
+                        </select>
+                    </div>
+                </div>
+                <div>
+                    <span>Start Game</span>
+                    <span className="material-symbols-outlined">flag</span>
+                </div>
              </div>
         </>
     )
