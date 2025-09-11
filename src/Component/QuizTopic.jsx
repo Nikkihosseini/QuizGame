491170c2c0eb454b3efdc15e4c93aaa9c7000d7d
Nikkihosseini <niekal00@gmail.com>
2025-09-11 19:19:04 +0330

    feat: add hover effect to QuizTopic using color and hoverColor props with onMouseEnter and onMouseLeave

--- src/Component/QuizTopic.jsx
+++ src/Component/QuizTopic.jsx
@@ -1,9 +1,14 @@
+import { useState } from "react";
 
+export default function QuizTopic({svg, topic, color, hoverColor}){
 
-export default function QuizTopic({svg,topic,color}){
+    const [bg, setBg] = useState(color);
+
     return(
         <>
-            <div className='flex flex-col items-center gap-3 text-secondary-color w-[110px] aspect-square p-2 rounded-xl cursor-pointer' style={{backgroundColor: color }}>
+            <div className='flex flex-col items-center gap-3 text-secondary-color w-[110px] aspect-square p-2 rounded-xl cursor-pointer' style={{backgroundColor: bg }}
+             onMouseEnter={() => setBg(hoverColor)}
+             onMouseLeave={() => setBg(color)}>
                 <span className="material-symbols-outlined text-6xl">{svg}</span>
                 <span className="text-lg">{topic}</span>
             </div>
