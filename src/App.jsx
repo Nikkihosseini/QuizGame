57967b6ec768239b8d24c6d268f2365a8201ab0a
Nikkihosseini <niekal00@gmail.com>
2025-09-19 10:52:33 +0330

    feat: setup react-router-dom with Home and QuizPage routes and navigate on Start

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
 import './index.css'
 import Home from "./Pages/Home"
+import QuestionPage from "./Pages/QuestionPage"
+import { BrowserRouter as Router , Routes, Route } from "react-router-dom";
 
 
 function App() {
 
   return (
     <>
-      <Home/>
+    <Router>
+      <Routes>
+
+        <Route path='/' element={<Home/>}/>
+        <Route path='/QuestionPage' element={<QuestionPage/>}/>
+      
+      </Routes>
+    </Router>
     </>
   )
 }
